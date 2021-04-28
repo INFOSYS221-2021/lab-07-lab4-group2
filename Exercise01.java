3534331111a9f33cddb5b275f4df0b2c07bb4dc3
bli621 <80798918+bli621@users.noreply.github.com>
2021-04-28 11:18:00 +0800

    Update Exercise01.java

--- Exercise01.java
+++ Exercise01.java
@@ -14,25 +14,41 @@ public class ExerciseOne {
     
     // Complete this method to return the word in all uppercase
     private String stringToUppercase(String word) {
-        return word;
+        int ll = word.length();
+        int i = 0;
+        String zzz ="";
+        while(i < ll){
+            char ch = word.charAt(i);
+            if(ch <= 122 && ch >= 97){  
+            ch -= 32;
+            zzz += ch;
+        }
+        i ++;
+        }
+        return zzz;
     }
     
     // Complete this method to return the length of the word
     private int wordLength(String word) {
-        return 0;
+        int k = word.length();
+        return k;
     }
     
     // Complete this method to return the index of 
     // the specified substring in the given word
     private int stringIndexOf(String subStr, String word) {
-        return 0;
+        
+        return word.indexOf(subStr);
     }
     
     // Complete this method to return a combination of 
     // the first three letters and the last four letters of the given word.
     // E.g. given the word "redorangeyellowgreen", "redgreen" should be returned
     private String wordCombo(String word) {
-        return word;
+        String rrr = "";
+        rrr += word.substring(0, 3);
+        rrr += word.substring(word.length()-5);
+        return rrr;
     }
     
     // Complete this method to return the frequencey of 
@@ -46,9 +62,14 @@ public class ExerciseOne {
         // go throuch each character in word
         // if the character is the same as the given letter,
         // then add 1 to freq
-        for (int i = 0; i < word.length(); i++) {
-            
-        }
-        return freq;
-    }
-}
+        if (word.contains(letter)) {
+            for(int iii = 0; iii < word.length(); iii++){
+                String aaa = word.charAt(iii);
+                if(aaa = letter){
+                    freq++;
+                } else{
+                return -1;
+                    }
+                }}
+
+        return freq;    }}
